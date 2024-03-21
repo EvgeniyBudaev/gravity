0ab4062a9c3e07c92317c44f99ca94f72fad8368
Evgeniy Budaev <budaev.e@gmail.com>
2024-03-21 06:26:38 +0300

    feat: config postgres

--- server-service/internal/config/config.go
+++ server-service/internal/config/config.go
@@ -11,11 +11,11 @@ type Config struct {
 	Port                string `envconfig:"PORT"`
 	LoggerLevel         string `envconfig:"LOGGER_LEVEL"`
 	Host                string `envconfig:"HOST"`
-	DBPort              string `envconfig:"DB_PORT"`
-	DBUser              string `envconfig:"DB_USER"`
-	DBPassword          string `envconfig:"DB_PASSWORD"`
-	DBName              string `envconfig:"DB_NAME"`
-	DBSSlMode           string `envconfig:"DB_SSLMODE"`
+	DBPort              string `envconfig:"POSTGRES_PORT"`
+	DBUser              string `envconfig:"POSTGRES_USER"`
+	DBPassword          string `envconfig:"POSTGRES_PASSWORD"`
+	DBName              string `envconfig:"POSTGRES_NAME"`
+	DBSSlMode           string `envconfig:"POSTGRES_SSLMODE"`
 	JWTSecret           string `envconfig:"JWT_SECRET"`
 	JWTIssuer           string `envconfig:"JWT_ISSUER"`
 	JWTAudience         string `envconfig:"JWT_AUDIENCE"`
