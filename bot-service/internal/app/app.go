ba3e3f3318b9cbe3dc98e977fdeac9d134c3fa9d
Evgeniy Budaev <budaev.e@gmail.com>
2024-03-28 18:22:15 +0300

    feat: bot-service rabbitmq

--- bot-service/internal/app/app.go
+++ bot-service/internal/app/app.go
@@ -3,7 +3,6 @@ package app
 import (
 	"github.com/EvgeniyBudaev/gravity/bot-service/internal/config"
 	"github.com/EvgeniyBudaev/gravity/bot-service/internal/logger"
-	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 )
 
@@ -28,47 +27,50 @@ func NewApp() *App {
 	if err != nil {
 		log.Fatal("error func NewApp, method NewLogger by path internal/app/app.go", err)
 	}
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		log.Fatal("error func NewApp, method amqp.Dial by path internal/app/app.go", err)
-	}
-	defer conn.Close()
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatal("error func NewApp, method conn.Channel by path internal/app/app.go", err)
-	}
-	defer ch.Close()
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	if err != nil {
-		log.Fatal("error func NewApp, method ch.QueueDeclare by path internal/app/app.go", err)
-	}
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	if err != nil {
-		log.Fatal("error func NewApp, method ch.Consume by path internal/app/app.go", err)
-	}
-	var forever chan struct{}
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-		}
-	}()
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
+	// RabbitMQ
+	//conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	//if err != nil {
+	//	log.Fatal("error func NewApp, method amqp.Dial by path internal/app/app.go", err)
+	//}
+	//defer conn.Close()
+	//ch, err := conn.Channel()
+	//if err != nil {
+	//	log.Fatal("error func NewApp, method conn.Channel by path internal/app/app.go", err)
+	//}
+	//defer ch.Close()
+	//queueName := "hello"
+	//q, err := ch.QueueDeclare(
+	//	queueName, // name
+	//	false,     // durable
+	//	false,     // delete when unused
+	//	false,     // exclusive
+	//	false,     // no-wait
+	//	nil,       // arguments
+	//)
+	//if err != nil {
+	//	log.Fatal("error func NewApp, method ch.QueueDeclare by path internal/app/app.go", err)
+	//}
+	//messages, err := ch.Consume(
+	//	q.Name, // queue
+	//	"",     // consumer
+	//	true,   // auto-ack
+	//	false,  // exclusive
+	//	false,  // no-local
+	//	false,  // no-wait
+	//	nil,    // args
+	//)
+	//if err != nil {
+	//	log.Fatal("error func NewApp, method ch.Consume by path internal/app/app.go", err)
+	//}
+	//var forever chan struct{}
+	//go func() {
+	//	for message := range messages {
+	//		log.Printf("received a message: %s", message.Body)
+	//	}
+	//}()
+	//log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	//<-forever
+	log.Printf("Starting http server...")
 	return &App{
 		Logger: loggerLevel,
 		config: cfg,
