99155c691cd3e4c8902db47431451ef6a9013bbf
Evgeniy Budaev <budaev.e@gmail.com>
2024-03-29 18:49:13 +0300

    feat: app

--- server-service/internal/app/app.go
+++ server-service/internal/app/app.go
@@ -1,14 +1,10 @@
 package app
 
 import (
-	"context"
 	"github.com/EvgeniyBudaev/gravity/server-service/internal/config"
 	"github.com/EvgeniyBudaev/gravity/server-service/internal/logger"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"time"
-
-	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 )
 
@@ -56,43 +52,43 @@ func NewApp() *App {
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 	}))
 	// RabbitMQ
-	conn, err := amqp.Dial("amqp://guest:guest@:15672/")
-	if err != nil {
-		log.Fatal("error func NewApp, method amqp.Dial by path internal/app/app.go", err)
-	}
-	defer conn.Close()
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatal("error func NewApp, method conn.Channel by path internal/app/app.go", err)
-	}
-	defer ch.Close()
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	if err != nil {
-		log.Fatal("error func NewApp, method ch.QueueDeclare by path internal/app/app.go", err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	body := "Hello World!"
-	err = ch.PublishWithContext(ctx,
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	if err != nil {
-		log.Fatal("error func NewApp, method ch.PublishWithContext by path internal/app/app.go", err)
-	}
-	log.Printf(" [x] Sent %s\n", body)
+	//conn, err := amqp.Dial("amqp://guest:guest@:15672/")
+	//if err != nil {
+	//	log.Fatal("error func NewApp, method amqp.Dial by path internal/app/app.go", err)
+	//}
+	//defer conn.Close()
+	//ch, err := conn.Channel()
+	//if err != nil {
+	//	log.Fatal("error func NewApp, method conn.Channel by path internal/app/app.go", err)
+	//}
+	//defer ch.Close()
+	//q, err := ch.QueueDeclare(
+	//	"hello", // name
+	//	false,   // durable
+	//	false,   // delete when unused
+	//	false,   // exclusive
+	//	false,   // no-wait
+	//	nil,     // arguments
+	//)
+	//if err != nil {
+	//	log.Fatal("error func NewApp, method ch.QueueDeclare by path internal/app/app.go", err)
+	//}
+	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//defer cancel()
+	//body := "Hello World!"
+	//err = ch.PublishWithContext(ctx,
+	//	"",     // exchange
+	//	q.Name, // routing key
+	//	false,  // mandatory
+	//	false,  // immediate
+	//	amqp.Publishing{
+	//		ContentType: "text/plain",
+	//		Body:        []byte(body),
+	//	})
+	//if err != nil {
+	//	log.Fatal("error func NewApp, method ch.PublishWithContext by path internal/app/app.go", err)
+	//}
+	//log.Printf(" [x] Sent %s\n", body)
 	log.Printf("Starting server service on port %s\n", cfg.Port)
 	return &App{
 		config: cfg,
