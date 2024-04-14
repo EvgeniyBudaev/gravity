ce6f3971fdd2acd3f9af1eb8ae5159321b57fdd7
Evgeniy Budaev <budaev.e@gmail.com>
2024-04-14 16:47:39 +0300

    fix: schemas

--- server-service/migrations/20240410061935_initSchema.up.sql
+++ server-service/migrations/20240410061935_initSchema.up.sql
@@ -1,113 +1,112 @@
-CREATE TABLE profiles
-(
+CREATE TABLE profiles (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    session_id VARCHAR NOT NULL,
-    display_name VARCHAR,
+    session_id VARCHAR NOT NULL UNIQUE,
+    display_name VARCHAR(255),
     birthday DATE,
-    gender VARCHAR,
-    location VARCHAR,
+    gender VARCHAR(100),
+    location TEXT,
     description TEXT,
-    height INTEGER NOT NULL,
-    weight INTEGER NOT NULL,
-    is_deleted BOOL NOT NULL,
-    is_blocked BOOL NOT NULL,
-    is_premium BOOL NOT NULL,
-    is_show_distance BOOL NOT NULL,
+    height INTEGER NOT NULL DEFAULT 0 CHECK(height >= 0),
+    weight INTEGER NOT NULL DEFAULT 0 CHECK(weight >= 0),
+    is_deleted BOOL NOT NULL DEFAULT false,
+    is_blocked BOOL NOT NULL DEFAULT false,
+    is_premium BOOL NOT NULL DEFAULT false,
+    is_show_distance BOOL NOT NULL DEFAULT false,
     is_invisible BOOL NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    last_online TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL CHECK (updated_at >= created_at),
+    last_online TIMESTAMP NOT NULL CHECK (last_online >= created_at)
 );
 
 CREATE TABLE profile_complaints (
-                                    id BIGSERIAL NOT NULL PRIMARY KEY,
-                                    profile_id BIGINT NOT NULL,
-                                    complaint_user_id BIGINT NOT NULL,
-                                    reason VARCHAR,
-                                    created_at TIMESTAMP NOT NULL,
-                                    updated_at TIMESTAMP NOT NULL,
-                                    CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    profile_id BIGINT NOT NULL,
+    complaint_user_id BIGINT NOT NULL,
+    reason TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL CHECK (updated_at >= created_at),
+    CONSTRAINT fk_profile_complaints_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
 );
 
 CREATE TABLE profile_telegram (
-                                  id BIGSERIAL NOT NULL PRIMARY KEY,
-                                  profile_id BIGINT,
-                                  telegram_id BIGINT,
-                                  username VARCHAR,
-                                  first_name VARCHAR,
-                                  last_name VARCHAR,
-                                  language_code VARCHAR,
-                                  allows_write_to_pm BOOL,
-                                  query_id VARCHAR,
-                                  chat_id BIGINT,
-                                  CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    profile_id BIGINT,
+    telegram_id BIGINT,
+    username VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    language_code VARCHAR,
+    allows_write_to_pm BOOL,
+    query_id TEXT,
+    chat_id BIGINT,
+    CONSTRAINT fk_profile_telegram_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
 );
 
 CREATE EXTENSION IF NOT EXISTS postgis;
 
 CREATE TABLE profile_navigators (
-                                    id BIGSERIAL NOT NULL PRIMARY KEY,
-                                    profile_id BIGINT NOT NULL,
-                                    location geometry(Point,  4326),
-                                    CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    profile_id BIGINT NOT NULL,
+    location geometry(Point,  4326),
+    CONSTRAINT fk_profile_navigators_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
 );
 
 CREATE TABLE profile_images (
-                                id BIGSERIAL NOT NULL PRIMARY KEY,
-                                profile_id BIGINT NOT NULL,
-                                name VARCHAR,
-                                url VARCHAR,
-                                size INTEGER,
-                                created_at TIMESTAMP NOT NULL,
-                                updated_at TIMESTAMP NOT NULL,
-                                is_deleted bool NOT NULL,
-                                is_blocked bool NOT NULL,
-                                is_primary bool NOT NULL,
-                                is_private bool NOT NULL,
-                                CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     profile_id BIGINT NOT NULL,
+     name VARCHAR(255),
+     url TEXT,
+     size INTEGER,
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP NULL CHECK (updated_at >= created_at),
+     is_deleted bool NOT NULL DEFAULT false,
+     is_blocked bool NOT NULL DEFAULT false,
+     is_primary bool NOT NULL DEFAULT false,
+     is_private bool NOT NULL DEFAULT false,
+     CONSTRAINT fk_profile_images_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
 );
 
 CREATE TABLE profile_filters (
-                                 id BIGSERIAL NOT NULL PRIMARY KEY,
-                                 profile_id BIGINT NOT NULL,
-                                 search_gender VARCHAR,
-                                 looking_for VARCHAR,
-                                 age_from INTEGER,
-                                 age_to INTEGER,
-                                 distance INTEGER,
-                                 page INTEGER,
-                                 size INTEGER,
-                                 CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     profile_id BIGINT NOT NULL,
+     search_gender VARCHAR(100),
+     looking_for VARCHAR(100),
+     age_from INTEGER,
+     age_to INTEGER,
+     distance INTEGER,
+     page INTEGER,
+     size INTEGER,
+     CONSTRAINT fk_profile_filters_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
 );
 
 CREATE TABLE profile_reviews (
-                                 id BIGSERIAL NOT NULL PRIMARY KEY,
-                                 profile_id BIGINT NOT NULL,
-                                 message TEXT,
-                                 rating DECIMAL(3,  1),
-                                 has_deleted BOOL NOT NULL,
-                                 has_edited BOOL NOT NULL,
-                                 created_at TIMESTAMP NOT NULL,
-                                 updated_at TIMESTAMP NOT NULL,
-                                 CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     profile_id BIGINT NOT NULL,
+     message TEXT,
+     rating DECIMAL(3,  1),
+     has_deleted BOOL NOT NULL DEFAULT false,
+     has_edited BOOL NOT NULL DEFAULT false,
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP NULL CHECK (updated_at >= created_at),
+     CONSTRAINT fk_profile_reviews_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
 );
 
 CREATE TABLE profile_likes (
-                               id BIGSERIAL NOT NULL PRIMARY KEY,
-                               profile_id BIGINT NOT NULL,
-                               likedUser_id BIGINT NOT NULL,
-                               is_liked BOOL NOT NULL,
-                               created_at TIMESTAMP NOT NULL,
-                               updated_at TIMESTAMP NOT NULL,
-                               CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     profile_id BIGINT NOT NULL,
+     likedUser_id BIGINT NOT NULL,
+     is_liked BOOL NOT NULL DEFAULT false,
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP NULL CHECK (updated_at >= created_at),
+     CONSTRAINT fk_profile_likes_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
 );
 
-CREATE TABLE profile_blocks(
-                               id BIGSERIAL NOT NULL PRIMARY KEY,
-                               profile_id BIGINT NOT NULL,
-                               blocked_user_id BIGINT NOT NULL,
-                               is_blocked BOOL NOT NULL,
-                               created_at TIMESTAMP NOT NULL,
-                               updated_at TIMESTAMP NOT NULL,
-                               CONSTRAINT fk_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
+CREATE TABLE profile_blocks (
+     id BIGSERIAL NOT NULL PRIMARY KEY,
+     profile_id BIGINT NOT NULL,
+     blocked_user_id BIGINT NOT NULL,
+     is_blocked BOOL NOT NULL DEFAULT false,
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP NULL CHECK (updated_at >= created_at),
+     CONSTRAINT fk_profile_blocks_profile_id FOREIGN KEY (profile_id) REFERENCES profiles (id)
 );
